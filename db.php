bb9598f3b623a7a5f919a31de58b7759bf608060
Lizeth-SP <119645514+Lizeth-SP@users.noreply.github.com>
2024-05-17 02:32:01 -0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+    //session_start();
+
+    $serverName = "servercomputo.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "bdComputo", // update me
+        "Uid" => "usr", // update me
+        "PWD" => "UsuarioLizeth1!" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+    /*if (isset($conn)){
+        echo 'Base de datos conectada';
+    }*/
+?>
